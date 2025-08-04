1afc1ddad93260371bb393c4fccdb9ee05e5edef
KEVIN ANDERSON MACHADO RAULINO <kevin.raulino@alunos.sc.senac.br>
2025-08-04 20:44:20 -0300

    commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>My first page in github pages - Kevin Raulino</h2>
+      <h3>um texto legal</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
